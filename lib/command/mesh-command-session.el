47ed2756997e6488cccd9cf9ca8887bb866665df
mytoh <mytoh@gmail.com>
2014-12-15 13:35:40 +0900

    * lib/command/mesh-command-session.el: :bug: add missig requirements

--- lib/command/mesh-command-session.el
+++ lib/command/mesh-command-session.el
@@ -2,6 +2,11 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+(require 'subr-x)
+
+(require 'mesh-core "lib/mesh-core")
+(require 'mesh-session "lib/mesh-session")
 
 ;;;###autoload
 (cl-defun mesh:new-session (new-session-name)
