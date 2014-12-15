2b734ca21f5ea910683125c0ef91082f04be6a17
mytoh <mytoh@gmail.com>
2014-12-15 12:58:29 +0900

    :lipstick: extract session creation to session--create

--- lib/command/mesh-command-session.el
+++ lib/command/mesh-command-session.el
@@ -6,37 +6,7 @@
 ;;;###autoload
 (cl-defun mesh:new-session (new-session-name)
   (interactive "sSession name: ")
-  (cl-letf ((new-session-name
-             (if (cl-find-if
-                  (lambda (session) (cl-equalp new-session-name
-                                          (mesh:get-name session)))
-                  (mesh:session-list))
-                 (concat new-session-name "*")
-               new-session-name)))
-    (cl-letf* ((current-session (mesh:current-session))
-               (current-session-tabs (mesh:get-tabs current-session))
-               (current-tab (mesh:get-current-tab current-session)))
-      (cl-letf* ((new-session current-session)
-                 (new-tab current-tab))
-        (oset new-tab :conf (current-window-configuration))
-        (oset new-session :tabs
-              (cl-subst new-tab current-tab current-session-tabs))
-        (setq mesh:*session-list*
-              (cl-subst new-session current-session
-                        (mesh:session-list)))))
-    (cl-letf* ((new-session (mesh:session--new
-                             new-session-name
-                             (mesh:session-list)))
-               (tab (car (mesh:get-tabs new-session)))
-               (conf (mesh:get-conf tab)))
-      (switch-to-buffer
-       (thread-first tab
-         mesh:get-panes
-         car
-         mesh:get-buffer))
-      (mesh:set-current-session new-session)
-      (setq mesh:*session-list*
-            (append (mesh:session-list) (list new-session))))))
+  (mesh:session--create new-session-name))
 
 (cl-defun mesh:kill-session ())
 (cl-defun mesh:next-session ())
--- lib/mesh-session.el
+++ lib/mesh-session.el
@@ -23,6 +23,39 @@
                      :current-tab new-tab
                      :tabs (list new-tab)))))
 
+(cl-defun mesh:sesssion--create (new-session-name)
+  (cl-letf ((new-session-name
+             (if (cl-find-if
+                  (lambda (session) (cl-equalp new-session-name
+                                          (mesh:get-name session)))
+                  (mesh:session-list))
+                 (concat new-session-name "*")
+               new-session-name)))
+    (cl-letf* ((current-session (mesh:current-session))
+               (current-session-tabs (mesh:get-tabs current-session))
+               (current-tab (mesh:get-current-tab current-session)))
+      (cl-letf* ((new-session current-session)
+                 (new-tab current-tab))
+        (oset new-tab :conf (current-window-configuration))
+        (oset new-session :tabs
+              (cl-subst new-tab current-tab current-session-tabs))
+        (setq mesh:*session-list*
+              (cl-subst new-session current-session
+                        (mesh:session-list)))))
+    (cl-letf* ((new-session (mesh:session--new
+                             new-session-name
+                             (mesh:session-list)))
+               (tab (car (mesh:get-tabs new-session)))
+               (conf (mesh:get-conf tab)))
+      (switch-to-buffer
+       (thread-first tab
+         mesh:get-panes
+         car
+         mesh:get-buffer))
+      (mesh:set-current-session new-session)
+      (setq mesh:*session-list*
+            (append (mesh:session-list) (list new-session))))))
+
 (provide 'mesh-session)
 
 ;;; mesh-session.el ends here
