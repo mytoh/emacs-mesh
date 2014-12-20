d2ddbe122ae58a9f376660bbd2b0629caf8712fc
mytoh <mytoh@gmail.com>
2014-12-20 23:39:44 +0900

    * lib/mesh-header-line.el: :heavy_plus_sign: add header line

---
+++ lib/mesh-header-line.el
@@ -0,0 +1,33 @@
+;;; mesh-header-line -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'eieio)
+
+(require 'mesh-core "lib/mesh-core")
+
+(defcustom mesh:header-line-format
+  `((:eval
+     (mesh:header-line)))
+  "header line for mesh")
+
+(cl-defun mesh:header-line ()
+  (cl-letf* ((current-session (mesh:current-session))
+             (current-session-list (mesh:session-list)))
+    (string-join
+     (seq-map
+      (lambda (session)
+        (if (cl-equalp session current-session)
+            (propertize
+             (mesh:get-name session)
+             'face 'font-lock-type-face)
+          (propertize
+           (mesh:get-name session)
+           'face 'font-lock-comment-face)))
+      current-session-list)
+     " ")))
+
+(provide 'mesh-header-line)
+
+;;; mesh-header-line.el ends here
