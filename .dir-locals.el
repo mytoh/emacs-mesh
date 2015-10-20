527a98247757db78b8d6e09a2e03188112331a23
mytoh <mytoh@gmail.com>
2015-10-20 16:54:31 +0900

    add nameless setting

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; .dir-locals --- .dir-locals -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+((emacs-lisp-mode .
+                  ((nameless-separator . ":")
+                   (nameless-current-name . "mesh")
+                   (eval . (nameless-mode)))))
+
+;;; .dir-locals.el ends here
