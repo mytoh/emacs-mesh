894cd12021a2eae13fe0a0684a6a4359c62706d3
mytoh <mytoh@gmail.com>
2017-04-14 00:00:10 +0900

    * lib/mesh-core.el: refactoring

--- lib/mesh-core.el
+++ lib/mesh-core.el
@@ -51,6 +51,12 @@
     (`[:tab ,tab] (mesh:find-prev-tab tab lst))
     (`[:session ,session] (mesh:find-prev-session session lst))))
 
+(cl-defun mesh:find-index (index things)
+  (colle:find
+   (lambda (x)
+     (eq index (glof:get x :index)))
+   things))
+
 (cl-defun mesh:find-next-pane (curpane panes)
   (cl-letf* ((indices (mesh:sort-numbers (mesh:get-indices panes)))
              (maxindex (seq-max indices))
@@ -62,15 +68,9 @@
     (pcase (seq-length panes)
       (1 nil)
       ((let `nil nextindex)
-       (seq-find
-        (lambda (pane)
-          (eq minindex (glof:get pane :index)))
-        panes))
+       (mesh:find-index minindex panes))
       ((let _ nextindex)
-       (seq-find
-        (lambda (pane)
-          (eq nextindex (glof:get pane :index)))
-        panes)))))
+       (mesh:find-index nextindex panes)))))
 
 (cl-defun mesh:find-next-tab (curtab tabs)
   (cl-letf* ((indices (mesh:sort-numbers (mesh:get-indices tabs)))
@@ -81,14 +81,8 @@
                                             maxindex
                                             indices)))
     (if nextindex
-        (seq-find
-         (lambda (tab)
-           (eq nextindex (glof:get tab :index)))
-         tabs)
-      (seq-find
-       (lambda (tab)
-         (eq minindex (glof:get tab :index)))
-       tabs))))
+        (mesh:find-index nextindex tabs)
+      (mesh:find-index minindex tabs))))
 
 (cl-defun mesh:find-next-session (cursession sessions)
   (cl-letf* ((cursessionpos (seq-position
@@ -121,7 +115,7 @@
 
 (cl-defun mesh:find-next-index-rec (index maxindex lst)
   (if (or (colle:empty-p lst)
-          (eq index maxindex))
+         (eq index maxindex))
       nil
     (cl-letf* ((target (1+ index))
                (next (seq-find (lambda (e) (cl-equalp e target))  lst)))
